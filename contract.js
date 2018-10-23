4bca74d9da27aca5a797a5aac889b8a0af275c14
Fabian Vogelsteller <fabian@frozeman.de>
2018-10-23 10:49:00 +0200

    add contarct.js

---
+++ contract.js
@@ -0,0 +1,70 @@
+// set contract
+var contract = new web3.eth.Contract([
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getBalance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "keys",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_myString",
+                "type": "string"
+            }
+        ],
+        "name": "setString",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getString",
+        "outputs": [
+            {
+                "name": "myRetString",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    }
+], '0xEC23B2C8b247570BF21EaF5fd2BFb8c36C066C70');
